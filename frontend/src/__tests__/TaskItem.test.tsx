5069a53d45dec244aa1c27f271132fc0292646d7
wyatt.t.putnam.mil@army.mil <wyatt.t.putnam.mil@army.mil>
2026-04-06 11:22:30 -0500

    Test for edit/save buttons

--- frontend/src/__tests__/TaskItem.test.tsx
+++ frontend/src/__tests__/TaskItem.test.tsx
@@ -1,11 +1,7 @@
 import {describe, it, expect, beforeAll, vi} from "vitest";
 import TaskTable from "../TaskTable.tsx";
-import {render, screen, within} from "@testing-library/react";
+import {fireEvent, render, screen, within} from "@testing-library/react";
 import type {Task} from "../TaskType.ts";
-import * as APIClient from "../APIClient.ts";
-import {isRunnableDevEnvironment} from "vite";
-import {Axios} from "axios";
-import {before} from "node:test";
 
 let task: Task;
 let taskTwo: Task;
@@ -64,107 +60,33 @@ describe('task item', () => {
     it('should display button', () => {
         render(<TaskTable tasks={tasks}/>);
         screen.logTestingPlaygroundURL();
-        const tableItems = screen.getAllByRole('row');
+        let tableItems = screen.getAllByRole('row');
         for (let i = 1; i<tableItems.length; i++){
             expect(within(tableItems[i]).getByRole('button', { name: /edit/i })).toBeInTheDocument();
         }
     });
-//     beforeAll(()=>{
-//         task= {
-//             title: "Test",
-//             description: "Testing implementation of tasks",
-//             isComplete: false,
-//             category:{
-//                 label:"Test Category",
-//                 id: 1
-//             },
-//             id: 1
-//         }
-//         taskTwo= {
-//             title: "TaskTwo",
-//             description: "Testing implementation of task two",
-//             isComplete: true,
-//             category:{
-//                 label:"Test Category Two",
-//                 id: 2
-//             },
-//             id: 2
-//         }
-//         tasks = [task];
-//         multipleTasks = [task, taskTwo];
-//     })
-//     it('should display task item', () => {
-//         //Arrange
-//         render(<TaskItem tasks={tasks} />)
-//         screen.logTestingPlaygroundURL();
-//         expect(screen.getByRole("cell", {name:/Title/i})).toBeInTheDocument();
-//         expect(screen.getByRole("cell", {name:/Description/i})).toBeInTheDocument();
-//         expect(screen.getByRole("cell", {name:/Status/i})).toBeInTheDocument();
-//         expect(screen.getByText("Category", {exact:true})).toBeInTheDocument();
-//     });
-//     it('should display task row', ()=>{
-//         render(<TaskItem tasks={tasks}/>)
-//         expect(screen.getByText("Test", {exact: true})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Testing implementation of tasks/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Yes|No/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Test Category/i})).toBeInTheDocument();
-//     })
-//     it('should display multiple tasks', () => {
-//         render(<TaskItem tasks={multipleTasks}/>)
-//         expect(screen.getByText("Test", {exact: true})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Testing implementation of task two/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/No/i})).toBeInTheDocument();
-//         expect(screen.getByText('Test Category', {exact: true})).toBeInTheDocument();
-//
-//         expect(screen.getByText("TaskTwo", {exact: true})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Testing implementation of tasks/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Yes/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Test Category Two/i})).toBeInTheDocument();
-//     });
-// });
-//
-// describe('API Client', () => {
-//     let data : Task[];
-//    beforeAll(async()=>{
-//        task= {
-//            title: "Test",
-//            description: "Testing implementation of tasks",
-//            isComplete: false,
-//            category:{
-//                label:"Test Category",
-//                id: 1
-//            },
-//            id: 1
-//        }
-//        taskTwo= {
-//            title: "TaskTwo",
-//            description: "Testing implementation of task two",
-//            isComplete: true,
-//            category:{
-//                label:"Test Category Two",
-//                id: 2
-//            },
-//            id: 2
-//        }
-//        tasks = [task];
-//        multipleTasks = [task, taskTwo];
-//
-//        vi.spyOn(APIClient, 'getTasks').mockResolvedValue(multipleTasks);
-//
-//        data = await APIClient.getTasks();
-//    });
-//     it('should display get tasks', () => {
-//         render(<TaskItem tasks={data}/>)
-//         expect(screen.getByText("Test", {exact: true})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Testing implementation of task two/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/No/i})).toBeInTheDocument();
-//         expect(screen.getByText('Test Category', {exact: true})).toBeInTheDocument();
-//
-//         expect(screen.getByText("TaskTwo", {exact: true})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Testing implementation of tasks/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Yes/i})).toBeInTheDocument();
-//         expect(screen.getByRole('cell', {name:/Test Category Two/i})).toBeInTheDocument();
-//     });
 
-
+    describe('task item button', () => {
+        it('should change row to input', () => {
+            render(<TaskTable tasks={tasks}/>)
+            let tableItems = screen.getAllByRole('row');
+            for (let i = 1; i<tableItems.length; i++){
+                let editButton = within(tableItems[i]).getByRole('button', { name: /edit/i });
+                //Click and make sure inputs appear
+                fireEvent.click(editButton);
+                let newTableItem = screen.getAllByRole('row')[i];
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].title}$`, "i"))).toBeInTheDocument();
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].description}$`, "i"))).toBeInTheDocument();
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].category.label}$`, "i"))).toBeInTheDocument();
+                expect(within(newTableItem).getByRole('radio')).toBeInTheDocument()
+                //Click save and make sure inputs disappear
+                let saveButton = within(newTableItem).getByRole('button', {name: /save/i});
+                fireEvent.click(saveButton);
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].title}$`, "i"))).not.toBeInTheDocument();
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].description}$`, "i"))).not.toBeInTheDocument();
+                expect(within(newTableItem).getByPlaceholderText(new RegExp(`^${tasks[i-1].category.label}$`, "i"))).not.toBeInTheDocument();
+                expect(within(newTableItem).getByRole('radio')).not.toBeInTheDocument()
+            }
+        });
+    });
 });
